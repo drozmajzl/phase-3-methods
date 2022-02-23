0e8551256c1d061b488b7fd980e9eaef9644cfe4
Daniel Rozmajzl <daniel.rozmajzl@gmail.com>
2022-02-23 16:34:12 -0500

    commit tree 00ef603b03c076f31fb59c9be6517b4938a4110d

--- methods.rb
+++ methods.rb
@@ -1,1 +1,23 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet name
+    puts "Hello, #{name}!"
+end
+
+def greet_with_default name = "programmer"
+    puts "Hello, #{name}!"
+end
+
+def add (n1, n2)
+    return n1 + n2
+end
+
+def halve num
+    if num.class != Integer
+        return nil
+    end
+    return num/2
+end
